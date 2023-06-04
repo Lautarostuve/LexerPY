eeeea0b006e2bef576094316cb2e4d20f32a4eed
MatiasBonesi <matiasbonesi@gmail.com>
2023-06-04 20:43:26 -0300

    automatas de si y sino

--- lexer.py
+++ lexer.py
@@ -1,2 +1,28 @@
-messimessimessileom
-MANCHESTER CITY CAMPEON
+def si (cadena):
+ estado_actual='A'
+ estado_final=['F']
+ for caracter in cadena:
+        if estado_actual=='A' and caracter=='s':
+         estado_actual ='B'
+        elif estado_actual=='B' and caracter=='i':
+          estado_actual='F'
+ if estado_actual in estado_final:
+  print('exito')
+ else:
+  print('no exito')
+def sino (cadena):
+  estado_actual='A'
+  estados_finales=['F']
+  for caracter in cadena:
+        if estado_actual=='A' and caracter=='s':
+         estado_actual ='B'
+        elif estado_actual=='B' and caracter=='i':
+          estado_actual='C'
+        elif estado_actual=='C' and caracter=='n':
+          estado_actual='D'
+        elif estado_actual=='D' and caracter=='o':
+          estado_actual='F'
+  if estado_actual in estado_final:
+    print('exito')
+  else:
+    print('no exito')
