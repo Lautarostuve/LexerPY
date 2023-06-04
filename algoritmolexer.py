868a8ec7f4a59f0e480e1e530ee66c10a79ff785
Lautarostuve <lautarostuve@gmail.com>
2023-06-04 17:41:34 -0300

    copia de hernan

--- algoritmolexer.py
+++ algoritmolexer.py
@@ -28,3 +28,40 @@ def automataid(lexema):
         return ESTADO_FINAL
     else:
         return ESTADO_NO_FINAL
+    
+    
+#hacer todos los automatas
+    
+    
+#algoritmo principal del lexer:
+
+def lexer(codigo_fuente):
+    tokens = [] #lista de tokens que va a devolver el lexer
+    posicion_actual = 0
+    while posicion_actual < len(codigo_fuente):
+        while codigo_fuente[posicion_actual].isspace():
+            posicion_actual = posicion_actual+1
+        
+        comienzo = posicion_actual
+        posibles_tokens = []
+        posibles_tokens_1mas = [] #posibles tokens si tomo el caracter siguiente
+        lexema = "" #inicia el lexema que pasa a los afd
+        todos_en_estado_trampa = False
+        
+        while not todos_en_estado_trampa:
+            todos_en_estado_trampa = True
+            lexema = codigo_fuente[comienzo:posicion_actual +1] #le paso del codigo fuente al lexema, las palabras desde el comienzo hasta la posicion actual +1
+            posibles_tokens = posibles_tokens_1mas
+            posibles_tokens_1mas = []
+            
+            for (un_tipo_de_token,afd) in TOKEN:
+                simulacion_afd = afd(lexema)
+                if simulacion_afd == ESTADO_FINAL:
+                    posibles_tokens_1mas.append(un_tipo_de_token)
+                    todos_en_estado_trampa= False
+                elif simulacion_afd == ESTADO_NO_FINAL:
+                    todos_en_estado_trampa= False
+                    
+            posicion_actual = posicion_actual + 1
+            
+            
