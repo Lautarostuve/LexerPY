5477a84fe40b8ef6543b7bd239f39d3dd39b5a40
Lautarostuve <lautarostuve@gmail.com>
2023-06-04 22:47:52 -0300

    automata palabras reservadas july3p

--- algoritmolexer.py
+++ algoritmolexer.py
@@ -6,7 +6,7 @@ TOKEN=[("tokenid",automataid),("tokennum",automatanum),
        ("tokenpalabrar",automatapalabrar),("tokenoprel",automataoprel),
        ("tokenopmat",automatamat), ("tokensigno",automatasigno)]
        
-#comienza la locura
+#ejemplo de automota facil
 
 def automataid(lexema):
     estado = 0 #actual
@@ -31,8 +31,129 @@ def automataid(lexema):
     
     
 #hacer todos los automatas
+
+#automata de palabras reservadas
+def automatapalabrar(lexema):
+    estado = 0
+    estados_finales = [2,4,12,17,24,20,31,36,40,47]
+    for caracter in lexema:
+        if estado == 0 and caracter == 's':
+            estado = 1
+        elif estado == 1 and caracter == 'i':
+            estado = 2 
+        elif estado == 2 and caracter == 'n':
+            estado = 3 
+        elif estado == 3 and caracter == 'o':
+            estado = 4
+            
+        elif estado == 0 and caracter == 'e':
+            estado = 5
+        elif estado == 5 and caracter == 'n':
+            estado = 6
+        elif estado == 6 and caracter == 't':
+            estado = 7
+        elif estado == 7 and caracter == 'o':
+            estado = 8
+        elif estado == 8 and caracter == 'n':
+            estado = 9
+        elif estado == 9 and caracter == 'c':
+            estado = 10
+        elif estado == 10 and caracter == 'e':
+            estado = 11
+        elif estado == 11  and caracter == 's':
+            estado = 12
+            
+        elif estado == 0 and caracter == 'f':
+            estado = 13
+        elif estado == 13 and caracter == 'i':
+            estado = 14
+        elif estado == 14 and caracter == 'n':
+            estado = 15
+        elif estado == 15 and caracter == 's':
+            estado = 16
+        elif estado == 16 and caracter == 'i':
+            estado = 17
+            
+        elif estado == 15 and caracter == 'f':
+            estado = 21
+        elif estado == 21 and caracter == 'u':
+            estado = 22
+        elif estado == 22 and caracter == 'n':
+            estado = 23
+        elif estado == 23 and caracter == 'c':
+            estado = 24
+            
+        elif estado == 0 and caracter == 'f':
+            estado =  13
+        elif estado == 13 and caracter == 'u':
+            estado = 18   
+        elif estado == 18 and caracter == 'n':
+            estado = 19   
+        elif estado == 19 and caracter == 'c':
+            estado = 20
+            
+        elif estado == 0  and caracter == 'r':
+            estado = 25
+        elif estado == 25  and caracter == 'e':
+            estado = 26
+        elif estado == 26 and caracter == 'p':
+            estado = 27
+        elif estado == 27 and caracter == 'e':
+            estado = 28
+        elif estado == 28 and caracter == 't':
+            estado = 29
+        elif estado == 29 and caracter == 'i':
+            estado = 30
+        elif estado == 30  and caracter == 'r':
+            estado = 31
+            
+        elif estado == 0  and caracter == 'h':
+            estado = 32
+        elif estado == 32 and caracter == 'a':
+            estado = 33
+        elif estado == 33 and caracter == 's':
+            estado = 34
+        elif estado == 34 and caracter == 't':
+            estado = 35
+        elif estado == 35 and caracter == 'a':
+            estado = 36
+            
+        elif estado == 0 and caracter == 'l':
+            estado = 37
+        elif estado == 37 and caracter == 'e':
+            estado = 38
+        elif estado == 38 and caracter == 'e':
+            estado = 39
+        elif estado == 39 and caracter == 'r':
+            estado = 40
+            
+        elif estado == 0 and caracter == 'm':
+            estado = 41
+        elif estado == 41  and caracter == 'o':
+            estado = 42
+        elif estado == 42 and caracter == 's':
+            estado = 43
+        elif estado == 43 and caracter == 't':
+            estado = 44
+        elif estado == 44 and caracter == 'r':
+            estado = 45
+        elif estado == 45 and caracter == 'a':
+            estado = 46
+        elif estado == 46 and caracter == 'r':
+            estado = 47
+            
+        else:
+            estado =-1
+            break
+
+    if estado == -1:
+        return ESTADO_TRAMPA       
+    if estado in estados_finales:
+        return ESTADO_FINAL
+    else:
+        return ESTADO_NO_FINAL
     
-    
+                       
 #algoritmo principal del lexer:
 
 def lexer(codigo_fuente):
